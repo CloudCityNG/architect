865a39213a9d8467f212d173c0e6a5a46db507eb
Atul Shimpi <shimpiatul@hotmail.com>
2017-05-10 01:32:48 +0530

    dd

---
+++ backend/config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'driver'    => 'mysql',
+	'host'      => 'localhost',
+	'database'  => 'builder',
+	'username'  => 'root',
+	'password'  => '',
+	'charset'   => 'utf8',
+	'prefix'    => '',
+	'collation' => 'utf8_unicode_ci',	
+);
